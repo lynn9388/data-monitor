23b4e6161db930da6ff9e0445e80077771d604a0
Lynn <lynn9388@gmail.com>
2016-05-20 00:16:13 +0800

    Add detail fragment's data update

--- app/src/main/java/com/lynn9388/datamonitor/fragment/DetailFragment.java
+++ app/src/main/java/com/lynn9388/datamonitor/fragment/DetailFragment.java
@@ -18,10 +18,11 @@
 
 package com.lynn9388.datamonitor.fragment;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -40,22 +41,29 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
 import com.lynn9388.datamonitor.R;
+import com.lynn9388.datamonitor.dao.TrafficLog;
 import com.lynn9388.datamonitor.util.TrafficUtil;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
+    private static int[] sRowViewIds = {R.id.row0, R.id.row1, R.id.row2, R.id.row3};
+    private static int[] sDataTypes = {R.string.mobile_down, R.string.mobile_up,
+            R.string.wifi_down, R.string.wifi_up};
     private static String[] sDataUsedPrefKeys = {"pref_key_mobile_down", "pref_key_mobile_up",
             "pref_key_wifi_down", "pref_key_wifi_up"};
-    private static int[] sDataTypeStringIds =
-            {R.string.mobile_down, R.string.mobile_up, R.string.wifi_down, R.string.wifi_up};
-    private static int[] sRowIds = {R.id.row0, R.id.row1, R.id.row2, R.id.row3};
+
     private PieChart mChart;
-    private View[] rows = new View[4];
+    private View[] mRows = new View[4];
+
+    private SharedPreferences mSharedPreferences;
+    private UpdateDataTask mUpdateDataTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,9 +77,12 @@ public class DetailFragment extends Fragment {
         mChart.setCenterTextSize(10f);
         mChart.animateY(3000, Easing.EasingOption.EaseInOutQuad);
 
-        for (int i = 0; i < sRowIds.length; i++) {
-            rows[i] = view.findViewById(sRowIds[i]);
+        for (int i = 0; i < sRowViewIds.length; i++) {
+            mRows[i] = view.findViewById(sRowViewIds[i]);
         }
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        updateViews();
 
         return view;
     }
@@ -79,12 +90,20 @@ public class DetailFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        updataData();
+        mUpdateDataTask = new UpdateDataTask();
+        mUpdateDataTask.execute();
     }
 
-    private void updataData() {
+    @Override
+    public void onPause() {
+        super.onPause();
+        mUpdateDataTask.cancel(true);
+    }
+
+    private void updateViews() {
         mChart.setCenterText(generatePieCenterText());
         mChart.setData(generatePieData());
+        mChart.invalidate();
 
         Legend legend = mChart.getLegend();
         int[] colors = legend.getColors();
@@ -92,11 +111,11 @@ public class DetailFragment extends Fragment {
         IPieDataSet dataSet = mChart.getData().getDataSet();
         float[] angles = mChart.getDrawAngles();
 
-        for (int i = 0; i < rows.length; i++) {
-            View color = rows[i].findViewById(R.id.color);
-            TextView dataType = (TextView) rows[i].findViewById(R.id.data_type);
-            TextView total = (TextView) rows[i].findViewById(R.id.total);
-            TextView percent = (TextView) rows[i].findViewById(R.id.percent);
+        for (int i = 0; i < mRows.length; i++) {
+            View color = mRows[i].findViewById(R.id.color);
+            TextView dataType = (TextView) mRows[i].findViewById(R.id.data_type);
+            TextView total = (TextView) mRows[i].findViewById(R.id.total);
+            TextView percent = (TextView) mRows[i].findViewById(R.id.percent);
 
             color.setBackgroundColor(colors[i]);
             dataType.setText(labels[i]);
@@ -106,10 +125,9 @@ public class DetailFragment extends Fragment {
     }
 
     private SpannableString generatePieCenterText() {
-        SharedPreferences localPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
         long total = 0L;
         for (String key : sDataUsedPrefKeys) {
-            total += localPreferences.getLong(key, 0L);
+            total += mSharedPreferences.getLong(key, 0L);
         }
         String readableValue = TrafficUtil.getReadableValue(total);
         SpannableString s = new SpannableString(readableValue + "\nTotal this month");
@@ -122,20 +140,18 @@ public class DetailFragment extends Fragment {
         ArrayList<Entry> entries1 = new ArrayList<>();
         ArrayList<String> xVals = new ArrayList<>();
 
-        for (int stringId : sDataTypeStringIds) {
+        for (int stringId : sDataTypes) {
             xVals.add(getString(stringId));
         }
 
-        SharedPreferences localPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
-        for (int i = 0; i < sDataTypeStringIds.length; i++) {
-            entries1.add(new Entry(localPreferences.getLong(sDataUsedPrefKeys[i], 2L), i));
+        for (int i = 0; i < sDataTypes.length; i++) {
+            entries1.add(new Entry(mSharedPreferences.getLong(sDataUsedPrefKeys[i], 0L), i));
         }
 
         PieDataSet dataSet = new PieDataSet(entries1, "Detail");
         int colors[] = {Color.rgb(217, 80, 138), Color.rgb(254, 149, 7),
                 Color.rgb(106, 167, 134), Color.rgb(53, 194, 209)};
         dataSet.setColors(colors);
-        dataSet.setSliceSpace(2f);
         dataSet.setValueTextColor(Color.WHITE);
         dataSet.setValueTextSize(12f);
 
@@ -143,5 +159,43 @@ public class DetailFragment extends Fragment {
         pieData.setValueFormatter(new PercentFormatter());
 
         return pieData;
+    }
+
+    private class UpdateDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            Date now = new Date();
+            Date start = TrafficUtil.getStartOfMonth(now);
+            Date end = TrafficUtil.getEndOfMonth(now);
+            List<TrafficLog> trafficLogs = TrafficUtil.getTrafficLogs(getContext(), start, end);
+
+            long mobileDown = 0;
+            long mobileUp = 0;
+            long wifiDown = 0;
+            long wifiUp = 0;
+            for (TrafficLog log : trafficLogs) {
+                mobileDown += log.getMobileRxBytes();
+                mobileUp += log.getMobileTxBytes();
+                wifiDown += log.getWifiRxBytes();
+                wifiUp += log.getWifiTxBytes();
+            }
+
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putLong(sDataUsedPrefKeys[0], mobileDown);
+            editor.putLong(sDataUsedPrefKeys[1], mobileUp);
+            editor.putLong(sDataUsedPrefKeys[2], wifiDown);
+            editor.putLong(sDataUsedPrefKeys[3], wifiUp);
+            editor.apply();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (!isCancelled()) {
+                updateViews();
+            }
+        }
     }
 }
