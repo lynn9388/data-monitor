e9e2a5fc17d8222f831809b162c6706b279cbf53
Lynn <lynn9388@gmail.com>
2016-05-29 04:39:20 +0800

    Fix direction when return from settings fragment

--- app/src/main/java/com/lynn9388/datamonitor/SettingsActivity.java
+++ app/src/main/java/com/lynn9388/datamonitor/SettingsActivity.java
@@ -20,6 +20,7 @@ package com.lynn9388.datamonitor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.lynn9388.datamonitor.fragment.SettingsFragment;
 
@@ -28,10 +29,19 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
